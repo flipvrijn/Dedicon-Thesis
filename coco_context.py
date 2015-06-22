445a0009274e1932d88bd62ee37c02ac32cc4c2f
Flip van Rijn <flipvrijn@gmail.com>
2015-06-22 10:21:04 +0200

    COCO dataset

--- coco_context.py
+++ coco_context.py
@@ -4,6 +4,22 @@ from urlparse import urlparse
 from progress.bar import Bar
 import os
 
+def fetch_flickr_data(img_url):
+    photo_id = os.path.split(urlparse(img_url).path)[1].split('_')[0]
+    ret = flickr.photos.getInfo(photo_id=photo_id)
+
+    if ret['stat'] == 'ok':
+        for u in ret['photo']['urls']['url']:
+            if u['type'] == 'photopage':
+                return {
+                    'url'        : u['_content'],
+                    'description': ret['photo']['description']['_content'],
+                    'title'      : ret['photo']['title']['_content'],
+                    'tags'       : [tag['_content'] for tag in ret['photo']['tags']['tag']]
+                }
+    else:
+        return None
+
 api_key      = u'0a5e2d1908aaf4580a0b1c4e430a6838'
 api_secret   = u'354c7458a637c929'
 dataset_path = 'fast-rcnn/data/coco/annotations/instances_train2014.json'
@@ -29,25 +45,25 @@ for img in coco_dataset['images']:
     try:
         img_id = str(img['id'])
         if img_id not in flickr_data.keys():
-            photo_id = os.path.split(urlparse(img['url']).path)[1].split('_')[0]
-            ret = flickr.photos.getInfo(photo_id=photo_id)
+            data = fetch_flickr_data(img['url'])
+            flickr_data[img_id] = data
 
-            if ret['stat'] == 'ok':
-                for u in ret['photo']['urls']['url']:
-                    if u['type'] == 'photopage':
-                        flickr_data[img_id] = {
-                            'url':         u['_content'],
-                            'description': ret['photo']['description']['_content'],
-                            'title':       ret['photo']['title']['_content'],
-                            'tags':        [tag['_content'] for tag in ret['photo']['tags']['tag']]
-                        }
-            else:
-                flickr_data[img_id] = None
+            if not data:
                 not_found_counter = not_found_counter + 1
                 print '\nERROR: Photo %s (%s) not found on Flickr! %d in total' % (photo_id, img_id, not_found_counter)
         else:
             if flickr_data[img_id] is None:
                 not_found_counter = not_found_counter + 1
+            else:
+                required_data = ['tags', 'description', 'title', 'url']
+                if len([i for i in required_data if i in flickr_data[img_id].keys()]) != len(required_data):
+                    print 'Missing some data, fetching it again...'
+                    data = fetch_flickr_data(img['url'])
+                    flickr_data[img_id] = data
+
+                    if not data:
+                        not_found_counter = not_found_counter + 1
+                        print '\nERROR: Photo %s (%s) not found on Flickr! %d in total' % (photo_id, img_id, not_found_counter)
         bar.next()
     except:
         # Interrupted and saving to file to, to continue later
--- coco_sanitize.py
+++ coco_sanitize.py
@@ -1,10 +1,63 @@
 import json
+from HTMLParser import HTMLParser
+import nltk
+from progress.bar import Bar
 
-f = 'input/flickr_data.json'
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return self.fed
 
-json_data = json.load(open(f, 'w'))
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
-for image_id in json_data['images']:
-    image_data = json_data['images'][image_id]
-    if image_data != None:
-        image_data = {}
+def sanitize_tags(tags):
+    return [sanitize_text(tag) for tag in tags]
+
+def sanitize_text(text):
+    return ' '.join(
+        [ token for token in nltk.word_tokenize(
+            ''.join(
+                [ fragment.replace('\n', '').strip().lower() for fragment in strip_tags(text) ]
+            )
+        ) if token.isalnum() or token in ['.', ',', '!', '?']]
+    )
+
+fd  = 'input/flickr_data.json'
+fcp = 'output/flickr_data_sanitized.cp.json'
+fo  = 'output/flickr_data_sanitized.json'
+
+print 'Loading Flickr JSON data...'
+flickr_data = json.load(open(fd, 'r'))
+
+bar = Bar('Sanitizing', max=len(flickr_data['images']), suffix='%(percent)d%%')
+for image_id in flickr_data['images']:
+    try:
+        image_data = flickr_data['images'][image_id]
+        if image_data != None:
+            # Check if all data is available
+            image_data = {
+                'tags'          : sanitize_tags(image_data['tags']),
+                'description'   : sanitize_text(image_data['description']),
+                'title'         : sanitize_text(image_data['title']),
+                'url'           : image_data['url']
+            }
+
+            flickr_data[image_id] = image_data
+        bar.next()
+    except:
+        # Interrupted and saving to file to, to continue later
+        print '\nInterrupted, saving to file...'
+        json.dump({'images': flickr_data}, open(fcp, 'w'))
+
+        raise
+bar.finish()
+
+print 'Writing Flickr data to file...'
+json.dump({'images': flickr_data}, open(fo, 'w'))
