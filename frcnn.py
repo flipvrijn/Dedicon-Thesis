2b8878b1727da14bc04be9c5117b0db43dc9eed5
Flip van Rijn <flipvrijn@gmail.com>
2015-07-21 16:20:42 +0200

    Finalized bbox detection

--- frcnn.py
+++ frcnn.py
@@ -9,6 +9,8 @@ from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
 from utils.cython_nms import nms
 from utils.timer import Timer
+import skimage.io
+from skimage.transform import resize
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
@@ -17,129 +19,173 @@ import argparse
 import hdf5storage
 import time
 import json
+import heapq
+import pandas as pd
 
 from IPython import embed
 
 CLASSES = ('__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
-#CLASSES = ('__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
 
-def apply_nms(all_boxes, thresh):
-    """Apply non-maximum suppression to all predicted boxes output by the
-    test_net method.
-    """
-    num_classes = len(all_boxes)
-    num_images = len(all_boxes[0])
-    nms_boxes = [[[] for _ in xrange(num_images)]
-                 for _ in xrange(num_classes)]
-    for cls_ind in xrange(num_classes):
-        for im_ind in xrange(num_images):
-            dets = all_boxes[cls_ind][im_ind]
-            if dets == []:
-                continue
-            keep = nms(dets, thresh)
-            if len(keep) == 0:
-                continue
-            nms_boxes[cls_ind][im_ind] = dets[keep, :].copy()
-    return nms_boxes
 
-def Detect(net, im, image_path, object_proposals, args):
-    """Detect object classes in an image assuming the whole image is an object."""
-    # Detect all object classes and regress object bounds
-    tic = time.time()
-    scores, boxes = im_detect(net, im, object_proposals)
-    toc = time.time()
-    detect_time = (toc-tic)
+def Detect(net, images, args):
+    """ Detect object classes in an image assuming the whole image is an object. """
+    proposals   = [read_mat('{}_boxes.mat'.format(im)) for im in images]
+    images      = [cv2.imread(im) for im in images]
+    num_images  = len(images)
 
-    # need to process each proposal
-    img_blob = {}
-    img_blob['img_path'] = image_path
-    img_blob['detections'] = []
-    img_blob['detect_time'] = detect_time 
-    # sort for each row
-    row_sort_idxs = np.argsort(-scores, axis = 1)
-    # remove background regions
-    if args.ignore_background:
-        row_sort_idxs = row_sort_idxs[row_sort_idxs != 0].reshape(row_sort_idxs.shape[0], row_sort_idxs.shape[1] - 1)
-    row_idxs_scores = scores[:,0].argsort()
-    sort_idxs = row_sort_idxs[row_idxs_scores][::-1][-10:]
+    for i in xrange(num_images):
+        print 'Detecting objects based on {} proposals...'.format(len(proposals[i]))
+        scores, boxes = im_detect(net, images[i], proposals[i])
 
-    sorted_scores = scores[row_idxs_scores]
-    sorted_boxes  = boxes[row_idxs_scores]
+        predictions_df = pd.DataFrame(scores, columns=CLASSES)
+        del predictions_df['__background__'] # Ignore background scores
+        max_scores = predictions_df.max(1) # Grab maximum scores over all regions
+        max_scores.sort(ascending=False) # ... sort descending
 
-    embed()
+        all_boxes = []
+        all_classes = []
+        for i in max_scores.index:
+            selected = predictions_df.iloc[i] 
+            selected.sort(ascending=False) 
+            cls = predictions_df.columns.get_loc(selected.index[0]) + 1 # Grab associated class
+            all_classes.append(cls)
+            cls_boxes = boxes[i, cls*4:(cls+1)*4] # Grab associated boxes
+            all_boxes.append(np.append(cls_boxes, max_scores[i]).astype(np.float32, copy=False))
 
-    # for each proposal
-    for idx, idx_rank in enumerate(sort_idxs):
-        idx_rank = idx_rank[:2] # a list
+        all_boxes = np.array(all_boxes)
+        all_classes = np.array(all_classes)
+        keep = nms(all_boxes, args.nms_thresh) # Apply non-maximum suppression
 
-        t_box = np.empty(1, dtype=np.object)
-        pred = np.empty(1, dtype=np.object)
-        conf = np.empty(1, dtype=np.float)
+        # Grab N 'best' locations
+        nms_boxes = all_boxes[keep][:args.num_regions]
+        nms_classes = all_classes[keep][:args.num_regions]
 
-        # TODO: select first element
-        t_box = sorted_boxes[idx, 4*idx_rank[0]:4*(idx_rank[0]+1)].tolist()
-        pred  = CLASSES[idx_rank[0]]
-        conf  = sorted_scores[idx, idx_rank[0]]
-   
-        img_blob['detections']  += [[t_box, pred, conf]]
+        out_boxes = np.hstack((nms_boxes, nms_classes[:, np.newaxis])).astype(np.float32, copy=False)
 
-    return detect_time, img_blob
-
+    return out_boxes
+    
 def read_mat(path):
+    """ Read HDF5 .mat file format. """
     data = hdf5storage.read(path='/', filename=path)
 
-    return data[0][0][0][0]
+    return data[0][0][0]
 
-def vis_detections(im, dets, args):
-    """Draw detected bounding boxes."""
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
+def vis_detections(all_boxes, images, rois, args):
+    """ Draw detected bounding boxes. """
+    for im_ind in xrange(len(images)):
+        im = skimage.io.imread(images[im_ind])
+        fig, ax = plt.subplots(figsize=(12, 12))
+        ax.imshow(im, aspect='equal')
 
-    for det in dets:
-        box, class_name, score = det
-        if score >= args.det_thresh:
+        classes = np.unique(all_boxes[:, -1:])
+        print 'Visualizing {} best locations with {} distinct classes...'.format(len(all_boxes), len(classes))
+        colormap = dict(zip(classes, plt.cm.Set1(np.linspace(0, 1, len(classes)))))
+
+        # Visualize the best regions
+        for box in all_boxes:
+            xmin, ymin, xmax, ymax, score, cls = box
+            
             ax.add_patch(
-                plt.Rectangle((box[0], box[1]),
-                              box[2] - box[0],
-                              box[3] - box[1], fill=False,
-                              edgecolor='red', linewidth=3.5)
+                plt.Rectangle((xmin, ymin),
+                               xmax - xmin,
+                               ymax - ymin, fill=False,
+                               edgecolor=colormap[cls], linewidth=3.5)
             )
-            ax.text(box[0], box[1] - 2,
-                    '{:s} {:.3f}'.format(class_name, score),
+            ax.text(xmin, ymin - 2,
+                    '{:s} {:.3f}'.format(CLASSES[int(cls)], score),
                     bbox=dict(facecolor='blue', alpha=0.5),
                         fontsize=14, color='white')
-    plt.title('{} most probable bounding boxes with {} classes'.format(len(dets), len(CLASSES)))
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
-    plt.show()
+        plt.axis('off')
+        plt.tight_layout()
+        plt.draw()
+
+        # Visualize the regions of interest separately...
+        fig_r, axes_r = plt.subplots(4, 6, figsize=(12, 12), subplot_kw={'xticks': [], 'yticks': []})
+        fig_r.subplots_adjust(hspace=0.3, wspace=0.05)
+
+        for ax, roi, cls, score in zip(axes_r.flat, rois[0][im_ind], rois[1][im_ind], rois[2][im_ind]):
+            ax.imshow(roi, interpolation='nearest', aspect='equal')
+            ax.set_title('{} ({:.3f})'.format(CLASSES[int(cls)], score))
+
+        plt.show()
+
+def get_rois(all_boxes, images):
+    """ Extracts regions of interest from image based on bounding boxes. """
+
+    rois        = np.empty((len(images), len(all_boxes)), dtype=object)
+    rois_cls    = np.empty((len(images), len(all_boxes)), dtype=int)
+    rois_score  = np.empty((len(images), len(all_boxes)), dtype=np.float32)
+
+    for im_ind in xrange(len(images)):
+        im = skimage.io.imread(images[im_ind])
+
+        for box_idx, box in enumerate(all_boxes):
+            xmin, ymin, xmax, ymax, score, cls = box
+
+            rois[im_ind][box_idx]       = im[ymin:ymax, xmin:xmax]
+            rois_cls[im_ind][box_idx]   = cls
+            rois_score[im_ind][box_idx] = score
+
+    return rois, rois_cls, rois_score
+
+def extract_cnn_features(net, rois, full):
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2, 0, 1))
+    transformer.set_raw_scale('data', 255)
+    transformer.set_channel_swap('data', (2, 1, 0))
+
+    feats = np.empty((len(rois) + 1, 4096))
+
+    # Extract features from RoIs ...
+    for roi_idx, roi in enumerate(rois):
+        net.blobs['data'].reshape(1, 3, 224, 224)
+        net.blobs['data'].data[...] = transformer.preprocess('data', roi)
+        out = net.forward()
+        feats[roi_idx] = out['fc7']
+
+    # ... and from the whole image
+    full = skimage.io.imread(full)
+    net.blobs['data'].reshape(1, 3, 224, 224)
+    net.blobs['data'].data[...] = transformer.preprocess('data', full)
+    out = net.forward()
+    feats[-1] = out['fc7']
+
+    return feats
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
     parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use', default=0, type=int)
-    #parser.add_argument('--def', dest='def', help='Network definition file')
-    #parser.add_argument('--net', dest='net', help='Network model file')
     parser.add_argument('--bg', dest='ignore_background', default=True, type=bool, help='Ignore/include background')
-    parser.add_argument('--det_thresh', dest='det_thresh', default=0.001, type=float, help='Object detection threshold')
+    parser.add_argument('-i', dest='img', type=str, help='Input image')
+    parser.add_argument('--nms_thresh', dest='nms_thresh', default=0.3, type=float, help='Non-maximum suppression threshold')
+    parser.add_argument('-r', dest='num_regions', default=19, type=int, help='Number of best regions in image')
 
     args = parser.parse_args()
 
-    prototxt = os.path.join(cfg.ROOT_DIR, 'models', 'VGG_CNN_M_1024', 'test-coco.prototxt')
-    model    = os.path.join(cfg.ROOT_DIR, 'output', 'default', 'coco_train2014', 'vgg_cnn_m_1024_fast_rcnn_iter_640000.caffemodel')
+    rcnn_prototxt = os.path.join(cfg.ROOT_DIR, 'models', 'VGG_CNN_M_1024', 'test-coco.prototxt')
+    rcnn_model    = os.path.join(cfg.ROOT_DIR, 'output', 'default', 'coco_train2014', 'vgg_cnn_m_1024_fast_rcnn_iter_640000.caffemodel')
+
+    cnn_prototxt  = os.path.join('models', 'VGG_ILSVRC_16_layers_deploy.prototxt')
+    cnn_model     = os.path.join(cfg.ROOT_DIR, 'data', 'imagenet_models', 'VGG16.v2.caffemodel')
 
     caffe.set_mode_gpu()
     caffe.set_device(args.gpu_id)
-    net = caffe.Net(prototxt, model, caffe.TEST)
+    rcnn_net = caffe.Net(rcnn_prototxt, rcnn_model, caffe.TEST)
+    cnn_net  = caffe.Net(cnn_prototxt, cnn_model, caffe.TEST)
 
     # Load the image
-    image_path = 'input/cars.jpg'
-    im = cv2.imread(image_path)
+    images = [args.img]
+    
+    # Detect the best locations in the image
+    all_boxes = Detect(rcnn_net, images, args)
 
-    box_file = os.path.join(image_path + '_boxes.mat')
-    obj_proposals = read_mat(box_file)
-    detect_time, dets = Detect(net, im, image_path, obj_proposals, args)
+    # Extract regions of interests
+    rois, rois_cls, rois_score = get_rois(all_boxes, images)
 
-    vis_detections(im, dets['detections'], args)
+    # Grab features from locations
+    feats = extract_cnn_features(cnn_net, rois[0], images[0])
 
-    plt.show()
+    # Visualize the detections
+    vis_detections(all_boxes, images, [rois, rois_cls, rois_score], args)
+
+    embed()
