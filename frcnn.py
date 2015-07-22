8a860de1626f4b6e790ebe7d16c2592a2e8f966b
Flip van Rijn <flipvrijn@gmail.com>
2015-07-22 11:55:01 +0200

    Refactored to run with multiple image files

--- frcnn.py
+++ frcnn.py
@@ -8,18 +8,14 @@ sys.path.insert(0, lib_path)
 from fast_rcnn.config import cfg 
 from fast_rcnn.test import im_detect
 from utils.cython_nms import nms
-from utils.timer import Timer
 import skimage.io
-from skimage.transform import resize
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.io as sio
-import caffe, cv2
+import caffe
 import argparse
 import hdf5storage
 import time
 import json
-import heapq
 import pandas as pd
 
 from IPython import embed
@@ -27,40 +23,39 @@ from IPython import embed
 CLASSES = ('__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
 
 
-def Detect(net, images, args):
+def Detect(net, image, args):
     """ Detect object classes in an image assuming the whole image is an object. """
-    proposals   = [read_mat('{}_boxes.mat'.format(im)) for im in images]
-    images      = [cv2.imread(im) for im in images]
-    num_images  = len(images)
+    proposals   = read_mat('{}_boxes.mat'.format(image))
+    image = skimage.io.imread(image)
 
-    for i in xrange(num_images):
-        print 'Detecting objects based on {} proposals...'.format(len(proposals[i]))
-        scores, boxes = im_detect(net, images[i], proposals[i])
+    print 'Detecting objects based on {} proposals...'.format(len(proposals))
+    scores, boxes = im_detect(net, image, proposals)
 
-        predictions_df = pd.DataFrame(scores, columns=CLASSES)
-        del predictions_df['__background__'] # Ignore background scores
-        max_scores = predictions_df.max(1) # Grab maximum scores over all regions
-        max_scores.sort(ascending=False) # ... sort descending
+    predictions_df = pd.DataFrame(scores, columns=CLASSES)
+    del predictions_df['__background__'] # Ignore background scores
+    max_scores = predictions_df.max(1) # Grab maximum scores over all regions
+    max_scores.sort(ascending=False) # ... sort descending
 
-        all_boxes = []
-        all_classes = []
-        for i in max_scores.index:
-            selected = predictions_df.iloc[i] 
-            selected.sort(ascending=False) 
-            cls = predictions_df.columns.get_loc(selected.index[0]) + 1 # Grab associated class
-            all_classes.append(cls)
-            cls_boxes = boxes[i, cls*4:(cls+1)*4] # Grab associated boxes
-            all_boxes.append(np.append(cls_boxes, max_scores[i]).astype(np.float32, copy=False))
+    dets_boxes   = []
+    dets_classes = []
+    for i in max_scores.index:
+        selected = predictions_df.iloc[i] 
+        selected.sort(ascending=False) 
+        cls = predictions_df.columns.get_loc(selected.index[0]) + 1 # Grab associated class
+        
+        dets_classes.append(cls)
+        cls_boxes = boxes[i, cls*4:(cls+1)*4] # Grab associated boxes
+        dets_boxes.append(np.append(cls_boxes, max_scores[i]).astype(np.float32, copy=False))
 
-        all_boxes = np.array(all_boxes)
-        all_classes = np.array(all_classes)
-        keep = nms(all_boxes, args.nms_thresh) # Apply non-maximum suppression
+    dets_boxes   = np.array(dets_boxes)
+    dets_classes = np.array(dets_classes)
+    keep         = nms(dets_boxes, args.nms_thresh) # Apply non-maximum suppression
 
-        # Grab N 'best' locations
-        nms_boxes = all_boxes[keep][:args.num_regions]
-        nms_classes = all_classes[keep][:args.num_regions]
+    # Grab N 'best' locations
+    nms_boxes   = dets_boxes[keep][:args.num_regions]
+    nms_classes = dets_classes[keep][:args.num_regions]
 
-        out_boxes = np.hstack((nms_boxes, nms_classes[:, np.newaxis])).astype(np.float32, copy=False)
+    out_boxes   = np.hstack((nms_boxes, nms_classes[:, np.newaxis])).astype(np.float32, copy=False)
 
     return out_boxes
     
@@ -70,61 +65,59 @@ def read_mat(path):
 
     return data[0][0][0]
 
-def vis_detections(all_boxes, images, rois, args):
+def vis_detections(boxes, image, rois, args):
     """ Draw detected bounding boxes. """
-    for im_ind in xrange(len(images)):
-        im = skimage.io.imread(images[im_ind])
-        fig, ax = plt.subplots(figsize=(12, 12))
-        ax.imshow(im, aspect='equal')
+    im = skimage.io.imread(image)
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.imshow(im, aspect='equal')
 
-        classes = np.unique(all_boxes[:, -1:])
-        print 'Visualizing {} best locations with {} distinct classes...'.format(len(all_boxes), len(classes))
-        colormap = dict(zip(classes, plt.cm.Set1(np.linspace(0, 1, len(classes)))))
+    classes = np.unique(boxes[:, -1:])
+    print 'Visualizing {} best locations with {} distinct classes...'.format(len(boxes), len(classes))
+    colormap = dict(zip(classes, plt.cm.Set1(np.linspace(0, 1, len(classes)))))
 
-        # Visualize the best regions
-        for box in all_boxes:
-            xmin, ymin, xmax, ymax, score, cls = box
-            
-            ax.add_patch(
-                plt.Rectangle((xmin, ymin),
-                               xmax - xmin,
-                               ymax - ymin, fill=False,
-                               edgecolor=colormap[cls], linewidth=3.5)
-            )
-            ax.text(xmin, ymin - 2,
-                    '{:s} {:.3f}'.format(CLASSES[int(cls)], score),
-                    bbox=dict(facecolor='blue', alpha=0.5),
-                        fontsize=14, color='white')
-        plt.axis('off')
-        plt.tight_layout()
-        plt.draw()
+    # Visualize the best regions
+    for box in boxes:
+        xmin, ymin, xmax, ymax, score, cls = box
+        
+        ax.add_patch(
+            plt.Rectangle((xmin, ymin),
+                           xmax - xmin,
+                           ymax - ymin, fill=False,
+                           edgecolor=colormap[cls], linewidth=3.5)
+        )
+        ax.text(xmin, ymin - 2,
+                '{:s} {:.3f}'.format(CLASSES[int(cls)], score),
+                bbox=dict(facecolor='blue', alpha=0.5),
+                    fontsize=14, color='white')
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
 
-        # Visualize the regions of interest separately...
-        fig_r, axes_r = plt.subplots(4, 6, figsize=(12, 12), subplot_kw={'xticks': [], 'yticks': []})
-        fig_r.subplots_adjust(hspace=0.3, wspace=0.05)
+    # Visualize the regions of interest separately...
+    fig_r, axes_r = plt.subplots(4, 6, figsize=(12, 12), subplot_kw={'xticks': [], 'yticks': []})
+    fig_r.subplots_adjust(hspace=0.3, wspace=0.05)
 
-        for ax, roi, cls, score in zip(axes_r.flat, rois[0][im_ind], rois[1][im_ind], rois[2][im_ind]):
-            ax.imshow(roi, interpolation='nearest', aspect='equal')
-            ax.set_title('{} ({:.3f})'.format(CLASSES[int(cls)], score))
+    for ax, roi, cls, score in zip(axes_r.flat, rois[0], rois[1], rois[2]):
+        ax.imshow(roi, interpolation='nearest', aspect='equal')
+        ax.set_title('{} ({:.3f})'.format(CLASSES[int(cls)], score))
 
-        plt.show()
+    plt.show()
 
-def get_rois(all_boxes, images):
+def get_rois(boxes, image):
     """ Extracts regions of interest from image based on bounding boxes. """
 
-    rois        = np.empty((len(images), len(all_boxes)), dtype=object)
-    rois_cls    = np.empty((len(images), len(all_boxes)), dtype=int)
-    rois_score  = np.empty((len(images), len(all_boxes)), dtype=np.float32)
+    rois        = np.empty(len(boxes), dtype=object)
+    rois_cls    = np.empty(len(boxes), dtype=int)
+    rois_score  = np.empty(len(boxes), dtype=np.float32)
 
-    for im_ind in xrange(len(images)):
-        im = skimage.io.imread(images[im_ind])
+    im = skimage.io.imread(image)
 
-        for box_idx, box in enumerate(all_boxes):
-            xmin, ymin, xmax, ymax, score, cls = box
+    for box_idx, box in enumerate(boxes):
+        xmin, ymin, xmax, ymax, score, cls = box
 
-            rois[im_ind][box_idx]       = im[ymin:ymax, xmin:xmax]
-            rois_cls[im_ind][box_idx]   = cls
-            rois_score[im_ind][box_idx] = score
+        rois[box_idx]       = im[ymin:ymax, xmin:xmax]
+        rois_cls[box_idx]   = cls
+        rois_score[box_idx] = score
 
     return rois, rois_cls, rois_score
 
@@ -159,6 +152,8 @@ if __name__ == '__main__':
     parser.add_argument('-i', dest='img', type=str, help='Input image')
     parser.add_argument('--nms_thresh', dest='nms_thresh', default=0.3, type=float, help='Non-maximum suppression threshold')
     parser.add_argument('-r', dest='num_regions', default=19, type=int, help='Number of best regions in image')
+    parser.add_argument('--viz', dest='viz', default=False, type=bool, help='Visualize region localization')
+    parser.add_argument('-o', dest='output', type=str, help='Output file')
 
     args = parser.parse_args()
 
@@ -175,17 +170,28 @@ if __name__ == '__main__':
 
     # Load the image
     images = [args.img]
-    
-    # Detect the best locations in the image
-    all_boxes = Detect(rcnn_net, images, args)
 
-    # Extract regions of interests
-    rois, rois_cls, rois_score = get_rois(all_boxes, images)
+    output = np.empty(len(images), dtype=object)
 
-    # Grab features from locations
-    feats = extract_cnn_features(cnn_net, rois[0], images[0])
+    for image_idx, image in enumerate(images):
+        print '[1/{}]: Putting {} into the R-CNN...'.format(len(images), image)
 
-    # Visualize the detections
-    vis_detections(all_boxes, images, [rois, rois_cls, rois_score], args)
+        # Detect the best locations in the image
+        boxes = Detect(rcnn_net, image, args)
 
-    embed()
+        print '[1/{}]: Extracting regions of interest...'.format(len(images))
+
+        # Extract regions of interest
+        rois, rois_cls, rois_score = get_rois(boxes, image)
+
+        print '[1/{}]: Grabbing features from regions of interest...'.format(len(images))
+
+        # Grab features from locations
+        output[image_idx] = extract_cnn_features(cnn_net, rois, image)
+
+        if args.viz:
+            # Visualize the detections
+            vis_detections(boxes, image, [rois, rois_cls, rois_score], args)
+
+    print 'Writing to file %s...' % args.output
+    hdf5storage.savemat(args.output, {'feats': output}, format='7.3', oned_as='row', store_python_metadata=True)
