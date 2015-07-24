7dd4fe5375134144e52f4dc91b7f7401cd7ba263
Flip van Rijn <flipvrijn@gmail.com>
2015-07-24 16:39:28 +0200

    Initial code for combining embedding

---
+++ networks/keras/sentembed.py
@@ -0,0 +1,138 @@
+import numpy
+from utils import *
+from itertools import izip, chain
+from collections import Counter
+import json
+
+from IPython import embed
+
+ftype = numpy.float32
+
+def one_hot(size, index):
+    a = numpy.zeros(size, dtype=ftype)
+    a[index] = 1
+    return a
+
+class SentEmbed(object):
+    '''
+        Object with a dictionary of {token: embedding value} pairs.
+    '''
+
+    invalid_token = "<<INVALID>>"
+    start_token   = "<<START>>"
+    end_token     = "<<END>>"
+
+    def __init__(self, infile, eol_tokens=None, min_count=5):
+        '''
+            Create KV embed from a tokenized (space-separated) file of sentences.
+        '''
+        with open(infile) as f:
+            data = json.load(open(infile, 'r'))
+
+        word_counts = {}
+        self.sentences = []
+        for image in data['images']:
+            for sentence in image['sentences']:
+                self.sentences.append([token.lower() for token in sentence['tokens']])
+                for word in sentence['tokens']:
+                    word = word.lower()
+                    word_counts[word] = word_counts.get(word, 0) + 1
+
+        words = [w for w in word_counts if word_counts[w] >= min_count]
+        alltokens = [self.invalid_token, self.start_token, self.end_token] + words
+
+        self.word_count = len(words)
+        self.special_token_count = len(alltokens) - len(words)
+        self.token_count = len(alltokens)
+
+        self.word_to_int = {word: i for i, word in enumerate(alltokens)}
+        self.int_to_word = {i: word for i, word in enumerate(alltokens)}
+
+        self.start = self.word_to_int[self.start_token]
+        self.end = self.word_to_int[self.end_token]
+        self.start_1h = one_hot(self.token_count, self.start)
+        self.end_1h = one_hot(self.token_count, self.start)
+        
+    def get(self, token):
+        '''
+            token -> index
+        '''
+        i = self.word_to_int.get(token, self.word_to_int[self.invalid_token])
+        return i
+
+    def get_1h(self, token):
+        '''
+            token -> one-hot vector
+        '''
+        i = self.word_to_int.get(token, self.word_to_int[self.invalid_token])
+        return one_hot(self.token_count, i)
+
+    def get_index(self, i):
+        '''
+            matrix index -> token
+        '''
+        return self.int_to_word[i]
+
+    def clip(self, sentence):
+        '''
+            Clips sentence at first occurrence of an eol token.
+            eol_tokens contains (token, inclusive=bool) pairs.
+        '''
+        for i, token in enumerate(sentence):
+            for eol, inclusive in self.eol_tokens:
+                if token == eol:
+                    return sentence[:i + int(inclusive)]
+
+        return sentence
+
+    # used to be convert_sentence
+    def sentence_to_1h(self, tokens, reverse=False, pad_length=None):
+        vectors = [self.get_1h(x) for x in tokens]
+        if pad_length:
+            vectors += [self.end_1h]*(pad_length-len(vectors))
+        if reverse:
+            vectors = list(reversed(vectors))
+        return vectors
+
+    def sentence_to_ids(self, tokens, reverse=False, pad_length=None):
+        vectors = [self.get(x) for x in tokens]
+        if pad_length:
+            vectors += [self.end]*(pad_length-len(vectors))
+        if reverse:
+            vectors = list(reversed(vectors))
+        return vectors
+
+    # used to be match_sentence
+    def vectors_to_sentence(self, vectors, clip=True):
+        tokens = [self.int_to_word[numpy.argmax(v)] for v in vectors]
+        if clip:
+            tokens = self.clip(tokens)
+        return tokens
+
+    def matchN(self, vector, n):
+        '''
+            Takes a probability vector of length token_count and returns the top n tokens.
+        '''
+        if len(vec) != self.token_count:
+            raise Exception("matchN: invalid input: expected vector of length {0} (received length {1})".format(self.token_count, len(vec)))
+
+        matches = [(self.int_to_word[i], i, prob) for i, prob in sorted(enumerate(vector), key=lambda (i, prob): prob, reverse=True)[:n]]
+        return tokens
+
+    def get_matrix(self, *args, **kwargs):
+        raise NotImplementedError("get_matrix: Not implemented")
+
+    def convert_sentence(self, *args, **kwargs):
+        raise NotImplementedError("convert_sentence: not implemented")
+
+    def match_sentence(self, *args, **kwargs):
+        raise NotImplementedError("match_sentence: not implemented")
+
+    def topN(self, *args, **kwargs):
+        raise NotImplementedError("topN: not implemented")
+
+    def match1(self, *args, **kwargs):
+        raise NotImplementedError("match1: not implemented")
+
+    def eol(self, *args, **kwargs):
+        raise NotImplementedError("eol: not implemented")
