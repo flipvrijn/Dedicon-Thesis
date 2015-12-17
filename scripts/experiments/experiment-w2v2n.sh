f84038d51198742b1033e7f89722aed08dd3ce64
Flip van Rijn <flipvrijn@gmail.com>
2015-12-17 09:17:43 +0100

    Added experiments scripts

---
+++ scripts/experiments/experiment-w2v2n.sh
@@ -0,0 +1,8 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../models/attention/" && pwd )"
+cd $DIR
+
+DATA_DIR="/media/Data/flipvanrijn/datasets/coco/processed/reduced/w2v2ngram/"
+OUT_DIR="/media/Data/flipvanrijn/models/"
+MODEL_NAME="context_attn_w2v2ngram.npz"
+
+python2.7 eval.py --attn_type=deterministic --type=t_attn $DATA_DIR $OUT_DIR $MODEL_NAME
---
+++ scripts/run-experiments.sh
@@ -0,0 +1,19 @@
+## Enable / Disable experiments by adding / removing a '#'
+
+# TF-IDF
+#sh ./experiments/experiment-tfidf.sh
+
+# W2V + TF-IDF
+#sh ./experiments/experiment-w2v1ntfidf.sh
+
+# W2V unigram (1n-gram)
+#sh ./experiments/experiment-w2v1ngram.sh
+
+# W2V bigram (2n-gram)
+sh ./experiments/experiment-w2v2ngram.sh
+
+# W2V trigram (3n-gram)
+#sh ./experiments/experiment-w2v3ngram.sh
+
+# RNN
+sh ./experiments/experiment-rnn.sh
